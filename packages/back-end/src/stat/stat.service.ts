32ff5038862a9e0e8ea3956256f5eb58e6688981
Scoheart <Scoheart1487790559@gmail.com>
2024-04-16 18:25:27 +0800

    chore: nothing

--- packages/back-end/src/stat/stat.service.ts
+++ packages/back-end/src/stat/stat.service.ts
@@ -4,23 +4,5 @@ import { UpdateStatDto } from './dto/update-stat.dto';
 
 @Injectable()
 export class StatService {
-  create(createStatDto: CreateStatDto) {
-    return 'This action adds a new stat';
-  }
-
-  findAll() {
-    return `This action returns all stat`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} stat`;
-  }
-
-  update(id: number, updateStatDto: UpdateStatDto) {
-    return `This action updates a #${id} stat`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stat`;
-  }
+  
 }
