1abbf201f4380a3746d6960812f50d8e5f5a77b8
Scoheart <Scoheart1487790559@gmail.com>
2024-04-18 13:53:51 +0800

    chore: nothing

--- packages/fe/src/constants/index.ts
+++ packages/fe/src/constants/index.ts
@@ -11,5 +11,10 @@ export const STAT_COLORS = [
   '#EAC100',
   '#FF9D6F',
 ];
-export const BASE_URL = 'http://39.101.76.177:8888';
-export const CLINET_URL = 'http://39.101.76.177:3000/question/';
+// 线上地址
+// export const BASE_URL = 'http://39.101.76.177:8888';
+// export const CLINET_URL = 'http://39.101.76.177:3000/question/';
+
+// 开发地址
+export const BASE_URL = 'http://localhost:8888';
+export const CLINET_URL = 'http://localhost:3000/question/';
