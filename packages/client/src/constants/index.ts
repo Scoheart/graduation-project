96c31464ceabf06ea2c00567b598f9fb11dd93eb
Scoheart <Scoheart1487790559@gmail.com>
2024-04-19 20:38:23 +0800

    chore: nothing

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -1,6 +1,10 @@
 // 线上地址
-export const BASE_URL = 'http://39.101.76.177:8888';
-export const CLINET_URL = 'http://39.101.76.177:3000';
+// export const BASE_URL = 'http://39.101.76.177:8888';
+// export const CLINET_URL = 'http://39.101.76.177:3000';
+
+// 线上地址2
+export const BASE_URL = 'http://118.178.180.11:8888';
+export const CLINET_URL = 'http://118.178.180.11:3000/question/';
 
 // 开发地址
 // export const BASE_URL = 'http://localhost:8888';
