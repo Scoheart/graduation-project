1335809e941f62b3193187fb933365a59f9a910d
Scoheart <Scoheart1487790559@gmail.com>
2024-04-19 13:51:40 +0800

    chore: nothing

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -1,7 +1,7 @@
 // 线上地址
-// export const BASE_URL = 'http://39.101.76.177:8888';
-// export const CLINET_URL = 'http://39.101.76.177:3000';
+export const BASE_URL = 'http://39.101.76.177:8888';
+export const CLINET_URL = 'http://39.101.76.177:3000';
 
 // 开发地址
-export const BASE_URL = 'http://localhost:8888';
-export const CLINET_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:8888';
+// export const CLINET_URL = 'http://localhost:3000';
--- packages/fe/src/constants/index.ts
+++ packages/fe/src/constants/index.ts
@@ -12,9 +12,9 @@ export const STAT_COLORS = [
   '#FF9D6F',
 ];
 // 线上地址
-// export const BASE_URL = 'http://39.101.76.177:8888';
-// export const CLINET_URL = 'http://39.101.76.177:3000/question/';
+export const BASE_URL = 'http://39.101.76.177:8888';
+export const CLINET_URL = 'http://39.101.76.177:3000/question/';
 
 // 开发地址
-export const BASE_URL = 'http://localhost:8888';
-export const CLINET_URL = 'http://localhost:3000/question/';
+// export const BASE_URL = 'http://localhost:8888';
+// export const CLINET_URL = 'http://localhost:3000/question/';
